b769fb8d32ca3eae01cf75b98ac224277a1e4bda
Harshit-sharma-cs21 <hrsharma8171087251@gmail.com>
2025-03-03 11:49:49 +0530

    added git commit -m added
    exit
    git commit -m added

---
+++ src/main/java/com/example/SecondSpringApp/beans/DepartmentBean.java
@@ -0,0 +1,17 @@
+package com.example.SecondSpringApp.beans;
+
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DepartmentBean {
+    private String deptName = "IT";
+
+    public String getDeptName() {
+        return deptName;
+    }
+
+    public void setDeptName(String deptName) {
+        this.deptName = deptName;
+    }
+}
